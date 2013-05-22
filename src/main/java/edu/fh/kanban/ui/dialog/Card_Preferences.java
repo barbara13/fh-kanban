4d4632243aaf86c7cb0823aa594aec26036ba419
Maxim Moshkov <mmoshkov@gmx.de>
2013-05-22 16:03:22 +0200

    am ticket gearbeitet

--- src/main/java/edu/fh/kanban/ui/dialog/Card_Preferences.java
+++ src/main/java/edu/fh/kanban/ui/dialog/Card_Preferences.java
@@ -1,13 +1,16 @@
 package edu.fh.kanban.ui.dialog;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
+import com.jgoodies.forms.builder.ButtonBarBuilder2;
 import com.jgoodies.forms.builder.DefaultFormBuilder;
 import com.jgoodies.forms.layout.CellConstraints;
 import com.jgoodies.forms.layout.FormLayout;
@@ -17,23 +20,18 @@ public class Card_Preferences implements ActionListener{
 	JTextField ticketID_textfield = new JTextField();
 	JTextField aufwand_textfield = new JTextField();
 	JTextField wert_textfield = new JTextField();
+	JTextArea beschreibung_text = new JTextArea();
 	JButton farb_button = new JButton("Farbe");
+	JButton create, cancel;
 	
 	public void actionPerformed(ActionEvent e) {
-		FormLayout formLayout = new FormLayout("right:p, 2dlu, p:g, 2dlu, p, 2dlu, right:p, 2dlu, p:g",	//colums
-											   "p, 1dlu, p, 5dlu, p, 2dlu, p , 2dlu, p, 2dlu");		//rows
+		FormLayout formLayout = new FormLayout("right:p, 2dlu, 5cm, 2cm, p, 4.8cm, right:p, 2dlu, 2cm",	//colums
+											   "p, 1dlu, p, 5dlu, p, 2dlu, p");							//rows
         CellConstraints cc = new CellConstraints();
-        formLayout.setColumnGroups(new int[][]{{1, 7}, {3, 9}});
+        //formLayout.setColumnGroups(new int[][]{{1, 7}, {3, 9}});
         
-        /*JPanel panel = new JPanel(formLayout);
-        panel.add(new JLabel("Ticket ID:"), cc.xy(1, 1));
-        panel.add(ticketID_textfield, cc.xy(3, 1));
-        panel.add(farb_button, cc.xy(5, 1));
-        panel.add(new JLabel("Aufwand"), cc.xy(7, 1));
-        panel.add(aufwand_textfield, cc.xy(9, 1));
-        panel.add(new JLabel("Wert"), cc.xy(7, 3));
-        panel.add(wert_textfield, cc.xy(9, 3));*/
-        
+        ButtonBarBuilder2 buttonbuilder = ButtonBarBuilder2.createLeftToRightBuilder();
+        buttonbuilder.addButton(new JButton[]{create = new JButton("create"), cancel = new JButton("cancel")});
         
         DefaultFormBuilder builder = new DefaultFormBuilder(formLayout);
         builder.setDefaultDialogBorder();
@@ -46,12 +44,14 @@ public class Card_Preferences implements ActionListener{
         builder.addLabel("Wert", cc.xy(7, 3));
         builder.add(wert_textfield, cc.xy(9, 3));
         builder.addSeparator("Beschreibung", cc.xyw(1, 5, 9));
+        builder.add(beschreibung_text, cc.xyw(1, 7, 9));
+        
+        beschreibung_text.setBackground(Color.gray);
         
         JFrame frame = new JFrame();
         frame.setLayout(new BorderLayout());
         frame.add(builder.getPanel(), BorderLayout.CENTER);
-        //frame.add(b.getPanel(), BorderLayout.SOUTH);
-        //frame.add(panel);
+        frame.add(buttonbuilder.getPanel(), BorderLayout.SOUTH);
         frame.setTitle("Ticket Erzeugen");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(700, 500);
