ddb83052ae3d2e692c94f4bdffa9da35092816f8
ploschka <b.ploschka@ostfalia.de>
2013-07-01 19:34:51 +0200

    laeuft

--- src/main/java/edu/fh/kanban/ui/dialog/Board_Preferences.java
+++ src/main/java/edu/fh/kanban/ui/dialog/Board_Preferences.java
@@ -1,194 +1,150 @@
-<<<<<<< HEAD
-package edu.fh.kanban.ui.dialog;
-import java.awt.BorderLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JSpinner;
-import javax.swing.JTextField;
-import com.jgoodies.forms.builder.DefaultFormBuilder;
-import com.jgoodies.forms.layout.CellConstraints;
-import com.jgoodies.forms.layout.FormLayout;
 
-public class Board_Preferences implements ActionListener{
-	
-	private JButton abbrechen, speichern;
-	private JFrame JFrame_BoardPreferences;
-		
-	JButton rot_Button = new JButton("rot");
-	JButton gelb_Button = new JButton("gelb");
-	JButton gruen_Button = new JButton("gruen");
-	JButton blau_Button = new JButton("blau");
-	JButton plus_Button = new JButton("+");
-	JButton minus_Button = new JButton("-");
-	
-	JTextField texteingabe_Bordname = new JTextField();
-	JTextField texteingabe_Colomsname = new JTextField();
-	
-	JSpinner auswahl = new JSpinner();
-	
-	public void actionPerformed(ActionEvent e){
-		FormLayout formLayout1 = new FormLayout("p, 2dlu, 3cm, 2dlu, p, 2dlu, 1cm, 2dlu, p, 2dlu, p, 2dlu, p, 2dlu, p, 2dlu, p, 2dlu, p, 2dlu, p ,2dlu, p ",  //Colums
-											   "p, 2dlu, p, 2dlu, p, 2dlu, p, 2dlu, p, 2dlu, p, 2dlu, p, 2dlu, p, 2dlu, p, 2dlu, p, 2dlu, p, 2dlu, p, 2dlu, p, 2dlu, p");	//rows
-        FormLayout formLayout2 = new FormLayout("center:2cm, 2dlu, center:2cm, 2dlu, center:2cm, 2dlu, center:2cm",
-        										"p");
-        FormLayout formLayout3 = new FormLayout("p, 2dlu, 3cm, 2dlu, p, 2dlu, p, 2dlu, p, 2dlu, p, 2dlu, p, 2dlu, p, 2dlu, p, 2dlu, p, 2dlu, p ,2dlu, p ",
-        										"p");
-        
-		CellConstraints cc = new CellConstraints();
-		formLayout2.setColumnGroups(new int[][]{{1, 3, 5, 7}, {}});
-		
-		DefaultFormBuilder bbutton = new DefaultFormBuilder(formLayout2); //Neues Register für Farben
-		bbutton.setDefaultDialogBorder();
-		
-		bbutton.add(rot_Button, cc.xy(1, 1));
-		bbutton.add(gelb_Button, cc.xy(3, 1));
-		bbutton.add(gruen_Button, cc.xy(5, 1));
-		bbutton.add(blau_Button, cc.xy(7, 1));
-                
-        DefaultFormBuilder builder = new DefaultFormBuilder(formLayout1);
-        builder.setDefaultDialogBorder();
-                      
-        builder.addLabel(" Bordname:", cc.xy(1, 1));
-        builder.add(texteingabe_Bordname, cc.xy(3,1));		//Textfeld Texteingabe	
-        
-        builder.addSeparator(" Color", cc.xyw(1, 3, 11));
-        
-        builder.add(bbutton.getPanel(), cc.xyw(1, 7, 11));
-        
-        builder.addSeparator(" Colums", cc.xyw(1, 9, 11) );
-        
-        //formLayout3.setColumnGroups(new int[][]{{1,3,5,7,9,11},{}});
-        
-        DefaultFormBuilder bbbutton = new DefaultFormBuilder(formLayout3);	//AB HIER
-        bbbutton.setDefaultDialogBorder();
-        
-        bbbutton.addLabel(" Name Colums:", cc.xy(1, 1));	//Label Name des Colums
-        bbbutton.add(texteingabe_Colomsname, cc.xy(3, 1));  //Textfeld_Texteingabe
-       	
-        bbbutton.addLabel("Wip", cc.xy(5, 1));
-        bbbutton.add(auswahl, cc.xy(7, 1));					//Erhöhen und senken der Wip
-        
-        bbbutton.add(plus_Button, cc.xy(9, 1));				//Plus Button um weitere Colums zu wählen
-        bbbutton.add(minus_Button, cc.xy(11, 1));			//Minus Button um erstellte Buttons zu löschen //BIS HIER SEPERAT
-               
-        builder.add(new JButton("Abbrechen"), cc.xy(5, 13));
-        builder.addActionListener(new ActionListener(){
-        	
-        	public void actionPerformed(new ActionEvent e){
-        		JFrame_BoardPreferences.dispose();
-        		}
-        }
-        builder.add(new JButton("Speichern"), cc.xy(9, 13));
-        builder.addActionListener(new ActionListener(){
-        	
-        	public void actionPerformed(new ActionEvent e){
-        		JFrame_BoardPreferences.dispose();
-        	}
-        }
-        
-        builder.add(bbbutton.getPanel(), cc.xyw(1,11, 11));
-        
-        JFrame frame = new JFrame();
-        frame.setLayout(new BorderLayout());
-        frame.add(builder.getPanel(), BorderLayout.CENTER);
-       
-        frame.setTitle("Board_Preferences erstellen");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(700, 500);
-        frame.setLocationByPlatform(true);
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
-	}
-        }
-=======
 package edu.fh.kanban.ui.dialog;
-import java.awt.BorderLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JSpinner;
-import javax.swing.JTextField;
-import com.jgoodies.forms.builder.DefaultFormBuilder;
-import com.jgoodies.forms.layout.CellConstraints;
-import com.jgoodies.forms.layout.FormLayout;
 
-public class Board_Preferences implements ActionListener{
-		
-	JButton rot_Button = new JButton("rot");
-	JButton gelb_Button = new JButton("gelb");
-	JButton gruen_Button = new JButton("gruen");
-	JButton blau_Button = new JButton("blau");
-	JButton plus_Button = new JButton("+");
-	JButton minus_Button = new JButton("-");
+import javax.swing.JFrame;
+import java.awt.Rectangle;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import com.jgoodies.forms.factories.FormFactory;
+import javax.swing.JLabel;
+import com.jgoodies.forms.factories.DefaultComponentFactory;
+import javax.swing.JRadioButton;
+import javax.swing.JTextField;
+import javax.swing.JSeparator;
+import javax.swing.JToggleButton;
+import javax.swing.JSpinner;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JScrollBar;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.ButtonGroup;
+
+
+
+public class Board_Preferences extends JFrame{
 	
-	JTextField texteingabe_Bordname = new JTextField();
-	JTextField texteingabe_Colomsname = new JTextField();
-	
-	JSpinner auswahl = new JSpinner();
-	
-	public void actionPerformed(ActionEvent e){
-		FormLayout formLayout1 = new FormLayout("p, 2dlu, 3cm, 2dlu, p, 2dlu, 1cm, 2dlu, p, 2dlu, p, 2dlu, p, 2dlu, p, 2dlu, p, 2dlu, p, 2dlu, p ,2dlu, p ",  //Colums
-											   "p, 2dlu, p, 2dlu, p, 2dlu, p, 2dlu, p, 2dlu, p, 2dlu, p, 2dlu, p, 2dlu, p, 2dlu, p, 2dlu, p, 2dlu, p, 2dlu, p, 2dlu, p");	//rows
-        FormLayout formLayout2 = new FormLayout("center:2cm, 2dlu, center:2cm, 2dlu, center:2cm, 2dlu, center:2cm",
-        										"p");
-        FormLayout formLayout3 = new FormLayout("p, 2dlu, 3cm, 2dlu, p, 2dlu, p, 2dlu, p, 2dlu, p, 2dlu, p, 2dlu, p, 2dlu, p, 2dlu, p, 2dlu, p ,2dlu, p ",
-        										"p");
-        
-		CellConstraints cc = new CellConstraints();
-		formLayout2.setColumnGroups(new int[][]{{1, 3, 5, 7}, {}});
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private JTextField txtName;
+	private JTextField txtColumsname;
+	private final ButtonGroup buttonGroup = new ButtonGroup();
+
+	public Board_Preferences(){
+		setBounds(new Rectangle(0, 0, 700, 500));
+		getContentPane().setLayout(new FormLayout(new ColumnSpec[] {
+				FormFactory.RELATED_GAP_COLSPEC,
+				ColumnSpec.decode("44px"),
+				FormFactory.RELATED_GAP_COLSPEC,
+				ColumnSpec.decode("69px"),
+				FormFactory.RELATED_GAP_COLSPEC,
+				ColumnSpec.decode("69px"),
+				FormFactory.RELATED_GAP_COLSPEC,
+				ColumnSpec.decode("69px"),
+				FormFactory.RELATED_GAP_COLSPEC,
+				ColumnSpec.decode("69px"),
+				ColumnSpec.decode("31px"),
+				ColumnSpec.decode("119px"),
+				FormFactory.RELATED_GAP_COLSPEC,
+				ColumnSpec.decode("125px"),},
+			new RowSpec[] {
+				FormFactory.RELATED_GAP_ROWSPEC,
+				RowSpec.decode("20px"),
+				RowSpec.decode("31px"),
+				RowSpec.decode("14px"),
+				RowSpec.decode("31px"),
+				RowSpec.decode("23px"),
+				RowSpec.decode("31px"),
+				RowSpec.decode("14px"),
+				FormFactory.RELATED_GAP_ROWSPEC,
+				RowSpec.decode("213px"),
+				RowSpec.decode("25px"),
+				RowSpec.decode("23px"),}));
 		
-		DefaultFormBuilder bbutton = new DefaultFormBuilder(formLayout2); //Neues Register für Farben
-		bbutton.setDefaultDialogBorder();
+		JLabel lblName = DefaultComponentFactory.getInstance().createLabel("Name:");
+		getContentPane().add(lblName, "2, 2, left, center");
 		
-		bbutton.add(rot_Button, cc.xy(1, 1));
-		bbutton.add(gelb_Button, cc.xy(3, 1));
-		bbutton.add(gruen_Button, cc.xy(5, 1));
-		bbutton.add(blau_Button, cc.xy(7, 1));
-                
-        DefaultFormBuilder builder = new DefaultFormBuilder(formLayout1);
-        builder.setDefaultDialogBorder();
-                      
-        builder.addLabel(" Bordname:", cc.xy(1, 1));
-        builder.add(texteingabe_Bordname, cc.xy(3,1));		//Textfeld Texteingabe	
-        
-        builder.addSeparator(" Color", cc.xyw(1, 3, 11));
-        
-        builder.add(bbutton.getPanel(), cc.xyw(1, 7, 11));
-        
-        builder.addSeparator(" Colums", cc.xyw(1, 9, 11) );
-        
-        //formLayout3.setColumnGroups(new int[][]{{1,3,5,7,9,11},{}});
-        
-        DefaultFormBuilder bbbutton = new DefaultFormBuilder(formLayout3);	//AB HIER
-        bbbutton.setDefaultDialogBorder();
-        
-        bbbutton.addLabel(" Name Colums:", cc.xy(1, 1));	//Label Name des Colums
-        bbbutton.add(texteingabe_Colomsname, cc.xy(3, 1));  //Textfeld_Texteingabe
-       	
-        bbbutton.addLabel("Wip", cc.xy(5, 1));
-        bbbutton.add(auswahl, cc.xy(7, 1));					//Erhöhen und senken der Wip
-        
-        bbbutton.add(plus_Button, cc.xy(9, 1));				//Plus Button um weitere Colums zu wählen
-        bbbutton.add(minus_Button, cc.xy(11, 1));			//Minus Button um erstellte Buttons zu löschen //BIS HIER SEPERAT
-               
-        builder.add(new JButton("Abbrechen"), cc.xy(5, 13));
-        builder.add(new JButton("Speichern"), cc.xy(9, 13));
-        
-        builder.add(bbbutton.getPanel(), cc.xyw(1,11, 11));
-        
-        JFrame frame = new JFrame();
-        frame.setLayout(new BorderLayout());
-        frame.add(builder.getPanel(), BorderLayout.CENTER);
-       
-        frame.setTitle("Teamproject 2013 - Kanban");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(700, 500);
-        frame.setLocationByPlatform(true);
-        frame.setVisible(true);    
+		txtName = new JTextField();
+		txtName.setText("");
+		getContentPane().add(txtName, "4, 2, fill, top");
+		txtName.setColumns(10);
+		
+		JLabel lblColors = DefaultComponentFactory.getInstance().createLabel("Colors:");
+		getContentPane().add(lblColors, "2, 4, fill, top");
+		
+		JSeparator separator = new JSeparator();
+		getContentPane().add(separator, "4, 4, 11, 1, fill, center");
+		
+		JToggleButton tglbtnRot = new JToggleButton("Rot");
+		buttonGroup.add(tglbtnRot);
+		getContentPane().add(tglbtnRot, "4, 6, fill, top");
+		
+		JToggleButton tglbtnGelb = new JToggleButton("Gelb");
+		buttonGroup.add(tglbtnGelb);
+		getContentPane().add(tglbtnGelb, "6, 6, fill, top");
+		
+		JToggleButton tglbtnGruen = new JToggleButton("Gruen");
+		buttonGroup.add(tglbtnGruen);
+		getContentPane().add(tglbtnGruen, "8, 6, fill, top");
+		
+		JToggleButton tglbtnBlau = new JToggleButton("Blau");
+		buttonGroup.add(tglbtnBlau);
+		getContentPane().add(tglbtnBlau, "10, 6, fill, top");
+		
+		JLabel lblColums = DefaultComponentFactory.getInstance().createLabel("Colums:");
+		getContentPane().add(lblColums, "2, 8, fill, top");
+		
+		JSeparator separator_1 = new JSeparator();
+		getContentPane().add(separator_1, "4, 8, 11, 1, fill, center");
+		
+		JPanel panel = new JPanel();
+		getContentPane().add(panel, "2, 10, 13, 1, fill, fill");
+		panel.setLayout(new FormLayout(new ColumnSpec[] {
+				FormFactory.RELATED_GAP_COLSPEC,
+				ColumnSpec.decode("44px"),
+				FormFactory.RELATED_GAP_COLSPEC,
+				ColumnSpec.decode("94px"),
+				FormFactory.RELATED_GAP_COLSPEC,
+				ColumnSpec.decode("44px"),
+				FormFactory.RELATED_GAP_COLSPEC,
+				ColumnSpec.decode("44px"),
+				FormFactory.RELATED_GAP_COLSPEC,
+				ColumnSpec.decode("44px"),
+				FormFactory.RELATED_GAP_COLSPEC,
+				ColumnSpec.decode("44px"),},
+			new RowSpec[] {
+				RowSpec.decode("31px"),
+				RowSpec.decode("23px"),}));
+		
+		JLabel lblName_1 = DefaultComponentFactory.getInstance().createLabel("Name");
+		panel.add(lblName_1, "2, 2, fill, center");
+		
+		txtColumsname = new JTextField();
+		panel.add(txtColumsname, "4, 2, fill, center");
+		txtColumsname.setText("Columsname");
+		txtColumsname.setColumns(10);
+		
+		JLabel lblWip = DefaultComponentFactory.getInstance().createLabel("Wip");
+		panel.add(lblWip, "6, 2, fill, center");
+		
+		JSpinner spinner = new JSpinner();
+		panel.add(spinner, "8, 2, fill, center");
+		
+		JButton button = new JButton("+");
+		panel.add(button, "10, 2, fill, top");
+		
+		JButton button_1 = new JButton("-");
+		panel.add(button_1, "12, 2, fill, top");
+		
+		JButton btnSpeichern = new JButton("Speichern");
+		getContentPane().add(btnSpeichern, "12, 12, fill, top");
+		
+		JButton btnAbbrechen = new JButton("Abbrechen");
+		getContentPane().add(btnAbbrechen, "14, 12, fill, top");
+		
+		
 	}
-        }
->>>>>>> 48607faa37e9c2e9af5732a45b9beedc450ba558
-	
+}
