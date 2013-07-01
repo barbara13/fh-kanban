f2e9b688e96817e9baec4e08a3ad41d03bc2d123
Maxim Moshkov <mmoshkov@gmx.de>
2013-07-01 21:19:07 +0200

    Maxim

--- src/main/java/edu/fh/kanban/ui/view/CardCreateView.java
+++ src/main/java/edu/fh/kanban/ui/view/CardCreateView.java
@@ -20,7 +20,7 @@ public class CardCreateView extends JFrame implements View{
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = 1L;	//Serial
+	private static final long serialVersionUID = 1L;
 	private JTextField txtCardId;
 	private JTextField txtAufwand;
 	private JTextField txtWert;
