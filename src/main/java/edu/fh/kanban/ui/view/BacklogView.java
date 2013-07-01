14d61fcf14cf54ca615e045996b6033e0ed8c7ba
Ghost1990 <lorenz.gassmann@web.de>
2013-07-01 22:55:50 +0200

    weitere GUI Tests

--- src/main/java/edu/fh/kanban/ui/view/BacklogView.java
+++ src/main/java/edu/fh/kanban/ui/view/BacklogView.java
@@ -6,9 +6,14 @@ import java.awt.GridLayout;
 import javax.swing.JComboBox;
 import net.miginfocom.swing.MigLayout;
 import javax.swing.JTextField;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import com.jgoodies.forms.factories.FormFactory;
+import javax.swing.JLabel;
 
 public class BacklogView implements View{
-	private JTextField txtSuche;
+	private JTextField txtSuche_1;
 
 	/**
 	 * @wbp.parser.entryPoint
@@ -16,12 +21,25 @@ public class BacklogView implements View{
 	@Override
 	public JComponent getComponent() {
 		JPanel panel = new JPanel();
-		panel.setLayout(new MigLayout("", "[][][][][][][][][][][][grow]", "[]"));
+		panel.setLayout(new FormLayout(new ColumnSpec[] {
+				ColumnSpec.decode("max(62dlu;pref)"),
+				FormFactory.RELATED_GAP_COLSPEC,
+				ColumnSpec.decode("max(121dlu;pref)"),
+				FormFactory.RELATED_GAP_COLSPEC,
+				ColumnSpec.decode("max(90dlu;pref):grow"),},
+			new RowSpec[] {
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,}));
 		
-		txtSuche = new JTextField();
-		txtSuche.setText("Suche");
-		panel.add(txtSuche, "cell 11 0,growx");
-		txtSuche.setColumns(10);
+		txtSuche_1 = new JTextField();
+		txtSuche_1.setText("Suche");
+		panel.add(txtSuche_1, "5, 3, fill, default");
+		txtSuche_1.setColumns(10);
 		return panel;
 	}
 
