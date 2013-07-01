9a2d28ece4ea96de0299cef7a71aee54695c8b64
Ghost1990 <lorenz.gassmann@web.de>
2013-07-01 22:05:41 +0200

    Backlog GUI Test

--- src/main/java/edu/fh/kanban/ui/view/BacklogView.java
+++ src/main/java/edu/fh/kanban/ui/view/BacklogView.java
@@ -2,12 +2,27 @@ package edu.fh.kanban.ui.view;
 
 import javax.swing.JComponent;
 import javax.swing.JPanel;
+import java.awt.GridLayout;
+import javax.swing.JComboBox;
+import net.miginfocom.swing.MigLayout;
+import javax.swing.JTextField;
 
 public class BacklogView implements View{
+	private JTextField txtSuche;
 
+	/**
+	 * @wbp.parser.entryPoint
+	 */
 	@Override
 	public JComponent getComponent() {
-		return new JPanel();
+		JPanel panel = new JPanel();
+		panel.setLayout(new MigLayout("", "[][][][][][][][][][][][grow]", "[]"));
+		
+		txtSuche = new JTextField();
+		txtSuche.setText("Suche");
+		panel.add(txtSuche, "cell 11 0,growx");
+		txtSuche.setColumns(10);
+		return panel;
 	}
 
 	
