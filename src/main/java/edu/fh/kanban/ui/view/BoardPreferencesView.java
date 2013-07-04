c68ec2bca6b7397d7f13897843ae5b6d5953c387
ploschka <b.ploschka@ostfalia.de>
2013-07-04 12:26:37 +0200

    Board Pref Done, nurnoch in DB speichern fehlt

--- src/main/java/edu/fh/kanban/ui/view/BoardPreferencesView.java
+++ src/main/java/edu/fh/kanban/ui/view/BoardPreferencesView.java
@@ -17,6 +17,7 @@ import com.jgoodies.forms.factories.DefaultComponentFactory;
 import edu.fh.kanban.database.Board;
 
 import javax.swing.JComponent;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.JSeparator;
 import javax.swing.JToggleButton;
@@ -64,70 +65,70 @@ public class BoardPreferencesView extends JFrame implements View{
 				ColumnSpec.decode("97px"),
 				FormFactory.UNRELATED_GAP_COLSPEC,
 				ColumnSpec.decode("97px"),
-				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.UNRELATED_GAP_COLSPEC,
 				ColumnSpec.decode("97px"),
 				FormFactory.RELATED_GAP_COLSPEC,
-				ColumnSpec.decode("max(21dlu;default)"),},
+				ColumnSpec.decode("33px"),},
 			new RowSpec[] {
 				FormFactory.RELATED_GAP_ROWSPEC,
 				RowSpec.decode("20px"),
-				RowSpec.decode("31px"),
-				RowSpec.decode("14px"),
-				RowSpec.decode("31px"),
-				RowSpec.decode("23px"),
-				RowSpec.decode("31px"),
-				RowSpec.decode("14px"),
 				FormFactory.RELATED_GAP_ROWSPEC,
-				RowSpec.decode("238px"),
-				RowSpec.decode("25px"),
+				RowSpec.decode("14px"),
+				FormFactory.UNRELATED_GAP_ROWSPEC,
+				RowSpec.decode("23px"),
+				FormFactory.UNRELATED_GAP_ROWSPEC,
+				RowSpec.decode("14px"),
+				FormFactory.UNRELATED_GAP_ROWSPEC,
+				RowSpec.decode("292px"),
+				FormFactory.UNRELATED_GAP_ROWSPEC,
 				RowSpec.decode("23px"),}));
 		
 		JLabel lblName = DefaultComponentFactory.getInstance().createLabel("Name: ");
-		getContentPane().add(lblName, "2, 2, left, center");
+		getContentPane().add(lblName, "2, 2, fill, center");
 		
 		txtName = new JTextField();
 		txtName.setText("");
-		getContentPane().add(txtName, "4, 2, left, top");
+		getContentPane().add(txtName, "4, 2, 11, 1, fill, top");
 		txtName.setColumns(10);
 		
 		JLabel lblColors = DefaultComponentFactory.getInstance().createLabel("Colors: ");
 		getContentPane().add(lblColors, "2, 4, fill, top");
 		
 		JSeparator separator = new JSeparator();
-		getContentPane().add(separator, "4, 4, 9, 1, fill, center");
+		getContentPane().add(separator, "4, 4, 11, 1, fill, bottom");
 		
-		JToggleButton tglbtnRot = new JToggleButton("Expedite");		//Button Rot
+		JToggleButton tglbtnRot = new JToggleButton("Expedite");
 		//tglbtnRot.setContentAreaFilled(false);
 		tglbtnRot.setOpaque(true);
 		tglbtnRot.setBackground(Color.RED);
 		getContentPane().add(tglbtnRot, "4, 6, fill, top");
 		
-		JToggleButton tglbtnGelb = new JToggleButton("Standard");		//Button Gelb
+		JToggleButton tglbtnGelb = new JToggleButton("Standard");
 		//tglbtnGelb.setContentAreaFilled(false);
 		tglbtnGelb.setOpaque(true);
 		tglbtnGelb.setBackground(Color.YELLOW);
 		getContentPane().add(tglbtnGelb, "6, 6, fill, top");
 		
-		JToggleButton tglbtnGruen = new JToggleButton("Fixed date");	//Button Grün
+		JToggleButton tglbtnGruen = new JToggleButton("Fixed date");
 		//tglbtnGruen.setContentAreaFilled(false);
 		tglbtnGruen.setOpaque(true);
 		tglbtnGruen.setBackground(Color.GREEN);
 		getContentPane().add(tglbtnGruen, "8, 6, fill, top");
 		
-		JToggleButton tglbtnBlau = new JToggleButton("Intangible");		//Button Blau
+		JToggleButton tglbtnBlau = new JToggleButton("Intangible");
 		//tglbtnBlau.setContentAreaFilled(false);
 		tglbtnBlau.setOpaque(true);
 		tglbtnBlau.setBackground(Color.BLUE);
-		getContentPane().add(tglbtnBlau, "10, 6, default, top");
+		getContentPane().add(tglbtnBlau, "10, 6, fill, top");
 		
 		JLabel lblColums = DefaultComponentFactory.getInstance().createLabel("Colums: ");
 		getContentPane().add(lblColums, "2, 8, fill, top");
 		
 		JSeparator separator_1 = new JSeparator();
-		getContentPane().add(separator_1, "4, 8, 9, 1, fill, center");
+		getContentPane().add(separator_1, "4, 8, 11, 1, fill, bottom");
 		
 		scrollPane = new JScrollPane();
-		getContentPane().add(scrollPane, "2, 10, 13, 1, fill, fill");
+		getContentPane().add(scrollPane, "2, 10, 13, 1, fill, top");
 		
 		panel = new JPanel();
 		scrollPane.setViewportView(panel);
@@ -184,7 +185,7 @@ public class BoardPreferencesView extends JFrame implements View{
 				dispose();
 			}
 		});
-		getContentPane().add(btnAbbrechen, "12, 12, fill, top");	
+		getContentPane().add(btnAbbrechen, "12, 12, default, top");	
 	}
 
 	public JComponent getComponent() {
@@ -198,8 +199,9 @@ public class BoardPreferencesView extends JFrame implements View{
 			btnMinus[arrayindex] = new JButton("-");
 			arrayindex++;
 			Aktualisierung();
-		} else
-			System.out.println("Mehr als 10 nicht moeglich!!!");
+		} else{
+			JOptionPane.showMessageDialog(null, "Nicht mehr als 10 erlaubt!!!", "Fehlermeldung", JOptionPane.WARNING_MESSAGE);	//Fehlermeldung wenn mehr als 10 Rows gewählt wurden
+		}
 	}
 	
 	private void Loeschung(int i){
