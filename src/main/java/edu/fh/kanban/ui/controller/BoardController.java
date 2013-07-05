81c978161e782bfc5a3118873039c2eb3e331389
davidfahr <david.fahr@gmx.net>
2013-07-05 17:00:09 +0200

    beispiel für Controller beim Board

--- src/main/java/edu/fh/kanban/ui/controller/BoardController.java
+++ src/main/java/edu/fh/kanban/ui/controller/BoardController.java
@@ -4,12 +4,33 @@
  */
 package edu.fh.kanban.ui.controller;
 import edu.fh.kanban.ui.view.BoardView;
+import java.awt.event.ActionEvent;
 import javax.swing.table.DefaultTableModel;
 
 /**
  *
  * @author David
  */
-public class BoardController{
-   
+
+public class BoardController extends Controller{
+    private Object src;
+    private BoardView bv;
+    
+    
+    public BoardController(BoardView bv){
+       this.bv = bv;
+       
+    }  
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        
+        src = e.getSource();
+        
+        //Getter - Methode für den Button und mit src vergleichen
+        if(src == bv.getCard1()){
+            System.out.println("test");
+        }
+        
+    }
 }
