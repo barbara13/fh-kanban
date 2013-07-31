c50e8ca9aa97e17c5a63422e2c6330bf04193ee3
Ghost1990 <lorenz.gassmann@web.de>
2013-07-31 14:09:40 +0200

    sort

---
+++ src/main/java/edu/fh/kanban/data/SizeComp.java
@@ -0,0 +1,26 @@
+/*package edu.fh.kanban.data;
+
+import java.util.Comparator;
+
+*//**
+ * @author Ghost
+ *
+ *//*
+public class SizeComp implements Comparator<Card>{
+
+	@Override
+	  public int compare(Card c1, Card c2) {
+	    int x=0;
+		if (c1.Integer.parseInt(Integer.parseInt(getValue())) == c2.Integer.parseInt(getValue())) {
+	      x= 0;										
+	    }
+		else if (c1.Integer.parseInt(getValue()) < c2.Integer.parseInt(getValue())) {		
+	      x= -1;									
+	    }
+		else if (c2.Integer.parseInt(getValue()) > c2.Integer.parseInt(getValue())) {
+	      x= 1;										
+	    }
+	    return x;
+
+}
+*/
